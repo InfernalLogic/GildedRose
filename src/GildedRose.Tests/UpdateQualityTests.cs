65189684a7656f021bbd53a9ad60beb0e6fa5895
Jakob Daniels <daniels.jakob@gmail.com>
2019-04-29 12:26:10 -0700

    added two tests for normal cases

---
+++ src/GildedRose.Tests/UpdateQualityTests.cs
@@ -0,0 +1,43 @@
+﻿using GildedRose.Console;
+using Xunit;
+
+namespace GildedRose.Tests
+{
+    public class UpdateQualityTests
+    {
+        [Fact]
+        public void QualityDegradesByOne_WhenSellByDateIsNotPast()
+        {
+            var item = new Item
+            {
+                Quality = 1,
+                SellIn = 1
+            };
+
+            Program.UpdateItem(item);
+            Assert.Equal(0, item.Quality);
+        }
+
+        [Fact]
+        public void SellInDegradesByOne_WhenSellByDateIsNotPast()
+        {
+            var item = new Item
+            {
+                Quality = 1,
+                SellIn = 1
+            };
+
+            Program.UpdateItem(item);
+            Assert.Equal(0, item.SellIn);
+        }
+
+        [Fact]
+        public void QualityDegradesTwiceAsFast_WhenSellByDateIsPast()
+        {
+            var item = new Item
+            {
+                
+            };
+        }
+    }
+}
